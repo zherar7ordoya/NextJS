f570016876ed10a9584b9f89f519298abf401d4e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-12-19 12:53:04 -0300

    Refactor ImageGallery to Rutina01 and add timer logic
    Renamed ImageGallery.jsx to Rutina01.jsx and updated App.jsx to use the new component. Added a timer and start button to Rutina01 for exercise routines, and improved CSS for layout and image display. Minor style adjustments were made in App.css and index.css for better container alignment and sizing.

--- pildoras-informaticas-2/src/App.jsx
+++ pildoras-informaticas-2/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ImageGallery from "./components/ImageGallery";
+import Rutina01 from "./components/Rutina01.jsx";
 
 function App() {
-    return <ImageGallery />;
+    return <Rutina01 />;
 }
 
 export default App;
--- pildoras-informaticas-2/src/components/Rutina01.jsx
+++ pildoras-informaticas-2/src/components/Rutina01.jsx
@@ -1,7 +1,8 @@
 import ejerciciosData from "../data/rutinas";
 import { useState } from "react";
 
-const ejercicioGallery = () => {
+const Rutina01 = () => {
+    // Destructure to get the first rutina where index is 0.
     const { titulo, rutina = [] } = ejerciciosData[0] || {
         titulo: "",
         rutina: [],
@@ -9,9 +10,32 @@ const ejercicioGallery = () => {
 
     const [clicked, setClicked] = useState(null);
 
+    const [time, setTime] = useState(0);
+
+    const beginner = () => {
+        while (true) {
+            rutina.map((ejercicio) => {
+                //
+                console.log(ejercicio);
+                //
+                setClicked(ejercicio);
+                for (let i = 5; i >= 0; i--) {
+                    //
+                    console.log(i);
+                    //
+                    setTime(i);
+                    const start = Date.now();
+                    while (Date.now() - start < 1000);
+                }
+            });
+        }
+    };
+
     return (
-        <section>
+        <section className="container">
             <h1>{titulo}</h1>
+            <button onClick={beginner}>Start</button>
+            <h2>{time}</h2>
             <div className="image-gallery">
                 {rutina.map((ejercicio, index) => (
                     <div key={index} className="image-item">
@@ -41,4 +65,4 @@ const ejercicioGallery = () => {
     );
 };
 
-export default ejercicioGallery;
+export default Rutina01;
