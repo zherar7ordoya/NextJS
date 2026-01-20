df2b85d256e0188f39cbb25073da8e16531a2f41
Gerardo Tordoya <zherar7ordoya@outlook.com>
2026-01-20 19:58:09 -0300

    Update React form example and add TypeScript
    Removed the action attribute and commented out the script in the HTML form intro. In the React form basics example, improved formatting and added TypeScript as a dev dependency. Added pnpm lock and workspace files for dependency management.

--- learn-react/03. React State/22. Form basics/index.jsx
+++ learn-react/03. React State/22. Form basics/index.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /**
  * Challenge: add another label and input for the password field
  */
 
 function App() {
-  return (
-    <section>
-      <h1>Signup form</h1>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
-        
-        
-      </form>
-    </section>
-  )
+    return (
+        <section>
+            <h1>Signup form</h1>
+            <form>
+                <label htmlFor="email">Email:</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="joe@schmoe.com"
+                />
+                <br />
+            </form>
+        </section>
+    );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
