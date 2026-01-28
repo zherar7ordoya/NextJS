5939327b3e0dcee7c0481bf3546b2a3c5c39abd3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2026-01-28 20:01:16 -0300

    000

--- learn-react/03. React State/55. Challenge - Get recipe from the AI chef/Main.jsx
+++ learn-react/03. React State/55. Challenge - Get recipe from the AI chef/Main.jsx
@@ -3,48 +3,20 @@ import IngredientsList from "./components/IngredientsList";
 import ClaudeRecipe from "./components/ClaudeRecipe";
 import { getRecipeFromMistral } from "./ai";
 
-/**
- * Challenge: Get a recipe from the AI!
- *
- * This will be a bit harder of a challenge that will require you
- * to think critically and synthesize the skills you've been
- * learning and practicing up to this point.
- *
- * Using either the `getRecipeFromChefClaude` function or the
- * `getRecipeFromMistral` function, make it so that when the user
- * clicks "Get a recipe", the text response from the AI is displayed
- * in the <ClaudeRecipe> component.
- *
- * For now, just have it render the raw markdown that the AI returns,
- * don't worry about making it look nice yet. (We're going to use a
- * package that will render the markdown for us soon.)
- */
-
 export default function Main() {
     const [ingredients, setIngredients] = React.useState([
+        "chicken",
         "all the main spices",
+        "corn",
+        "heavy cream",
         "pasta",
-        "ground beef",
-        "tomato paste",
     ]);
-    const [recipeShown, setRecipeShown] = React.useState(false);
     const [recipe, setRecipe] = React.useState("");
-    const [isLoading, setIsLoading] = React.useState(false);
 
-    React.useEffect(() => {
-        if (recipeShown) {
-            async function fetchRecipe() {
-                setIsLoading(true);
-                const recipeText = await getRecipeFromMistral(ingredients);
-                setRecipe(recipeText);
-                setIsLoading(false);
-            }
-            fetchRecipe();
-        }
-    }, [recipeShown]);
-
-    function toggleRecipeShown() {
-        setRecipeShown((prevShown) => !prevShown);
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients);
+        console.log(recipeMarkdown);
+        setRecipe(recipeMarkdown);
     }
 
     function addIngredient(formData) {
@@ -70,16 +42,11 @@ export default function Main() {
             {ingredients.length > 0 && (
                 <IngredientsList
                     ingredients={ingredients}
-                    toggleRecipeShown={toggleRecipeShown}
+                    getRecipe={getRecipe}
                 />
             )}
 
-            {recipeShown &&
-                (isLoading ? (
-                    <p className="loading">Getting recipe...</p>
-                ) : (
-                    <ClaudeRecipe recipe={recipe} />
-                ))}
+            {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     );
 }
