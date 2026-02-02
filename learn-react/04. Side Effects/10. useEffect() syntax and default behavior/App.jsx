ed092ded6a42bdf2b3314084a5076e4f90245d2f
Gerardo Tordoya <zherar7ordoya@outlook.com>
2026-02-02 20:01:07 -0300

    ...

--- learn-react/04. Side Effects/10. useEffect() syntax and default behavior/App.jsx
+++ learn-react/04. Side Effects/10. useEffect() syntax and default behavior/App.jsx
@@ -1,17 +1,20 @@
-import React from "react"
+import React from "react";
 
 export default function App(props) {
-    const [starWarsData, setStarWarsData] = React.useState(null)
-    
-    // console.log("Rendered!")
-    
-    fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
-    
+    const [starWarsData, setStarWarsData] = React.useState(null);
+
+    console.log("Rendered in the first place!");
+
+    React.useEffect(function () {
+        console.log("Effect run in the second place!");
+
+        fetch("https://swapi.dev/api/people/1").then((res) => res.json());
+        //.then(data => setStarWarsData(data))
+    });
+
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
-    )
+    );
 }
